b87d15d03f7350e79720cbbc263303a59f077892
SofiaKubo <skuboreva@gmail.com>
2025-12-15 18:39:55 +0100

    solve 1-task from 1-module

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,27 @@
 function factorial(n) {
-  // ваш код...
+  if (typeof n !== "number") {
+    throw new Error("n должно быть числом");
+  }
+
+  if (!Number.isFinite(n)) {
+    throw new Error("n должно быть конечным числом");
+  }
+
+  if (!Number.isInteger(n)) {
+    throw new Error("n должно быть целым числом");
+  }
+
+  if (n < 0) {
+    throw new Error("n должно быть неотрицательным");
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
